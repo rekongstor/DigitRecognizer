66d5d9274b1da187e507410c241da3868da42261
Rekongstor <rekongstor@mail.ru>
2020-02-09 00:50:29 +0300

    Перенесены WinForms в отдельный класс

--- DigitRecognizer/Window.h
+++ DigitRecognizer/Window.h
@@ -1,6 +1,11 @@
 #pragma once
+#include <windows.h>
 
 class Window
 {
-	Window 
+	static Window window;
+	static LRESULT OnUpdate(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	Window();
+public:
+	static int OnInit(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow);
 };
