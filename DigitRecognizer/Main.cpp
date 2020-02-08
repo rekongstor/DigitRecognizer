5694bc9e1d2548ded8f1b65c3e219f9203c1e60a
Reko-laptop <intreface@yandex.com>
2020-02-08 14:11:19 +0300

    Добавлена инициализация DirectX и обновление фреймов. Пока что по грубой реализации из тутора.

--- DigitRecognizer/Main.cpp
+++ DigitRecognizer/Main.cpp
@@ -2,6 +2,8 @@
 main с загрузкой датасета в память
 */
 #include <windows.h>
+#include <d3d11.h>
+#pragma comment (lib, "d3d11.lib")
 
 #include "SingleCoreDG.h"
 
@@ -19,16 +21,101 @@ int digit_recognizer()
 }
 
 
-void OnPaint(HWND hWnd)
+IDXGISwapChain* swapchain;             // the pointer to the swap chain interface
+ID3D11Device* dev;                     // the pointer to our Direct3D device interface
+ID3D11DeviceContext* devcon;           // the pointer to our Direct3D device context	
+ID3D11RenderTargetView* backbuffer;    // the pointer to our back buffer
+
+// this function initializes and prepares Direct3D for use
+void InitD3D(HWND hWnd)
 {
-	PAINTSTRUCT  ps;
-	HDC          hdc;
-	InvalidateRect(hWnd, NULL, TRUE);
-	hdc = BeginPaint(hWnd, &ps);
-	EndPaint(hWnd, &ps);
+	// create a struct to hold information about the swap chain
+	DXGI_SWAP_CHAIN_DESC scd;
+
+	// clear out the struct for use
+	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	// fill the swap chain description struct
+	scd.BufferCount = 1;                                    // one back buffer
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
+	scd.OutputWindow = hWnd;                                // the window to be used
+	scd.SampleDesc.Count = 1;                               // how many multisamples
+	scd.SampleDesc.Quality = 0;                             // multisample quality level
+	scd.Windowed = TRUE;                                    // windowed/full-screen mode
+
+	// create a device, device context and swap chain using the information in the scd struct
+	D3D11CreateDeviceAndSwapChain(NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		D3D11_SDK_VERSION,
+		&scd,
+		&swapchain,
+		&dev,
+		NULL,
+		&devcon);
+
+
+	// get the address of the back buffer
+	ID3D11Texture2D* pBackBuffer;
+	swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+	// use the back buffer address to create the render target
+	dev->CreateRenderTargetView(pBackBuffer, NULL, &backbuffer);
+	pBackBuffer->Release();
+
+	// set the render target as the back buffer
+	devcon->OMSetRenderTargets(1, &backbuffer, NULL);
+
+
+	// Set the viewport
+	D3D11_VIEWPORT viewport;
+	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = 800;
+	viewport.Height = 600;
+
+	devcon->RSSetViewports(1, &viewport);
+}
+// this is the function that cleans up Direct3D and COM
+void CleanD3D()
+{
+	// close and release all existing COM objects
+	swapchain->Release();
+	backbuffer->Release();
+	dev->Release();
+	devcon->Release();
+}
+void RenderFrame(void)
+{
+	// clear the back buffer to a deep blue
+	float color[] = { 0.0f, 0.2f, 0.4f, 1.0f };
+	devcon->ClearRenderTargetView(backbuffer, color);
+
+	// do 3D rendering on the back buffer here
+
+	// switch the back buffer and the front buffer
+	swapchain->Present(0, 0);
 }
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
+	WPARAM wParam, LPARAM lParam)
+{
+	//HDC          hdc;
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+}
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
 {
@@ -44,7 +131,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
 	wndClass.hInstance = hInstance;
 	wndClass.hIcon = NULL;
 	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
 	wndClass.lpszMenuName = NULL;
 	wndClass.lpszClassName = TEXT("DigitRecognizer");
 
@@ -64,30 +151,16 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
 		NULL);                    // creation parameters
 
 	ShowWindow(hWnd, iCmdShow);
+	InitD3D(hWnd);
 	UpdateWindow(hWnd);
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
+		RenderFrame();
 	}
+	CleanD3D();
 
-	return msg.wParam;
+	return (int)msg.wParam;
 }
 
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
-	WPARAM wParam, LPARAM lParam)
-{
-	HDC          hdc;
-	switch (message)
-	{
-	case WM_PAINT:
-		OnPaint(hWnd);
-		return 0;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-}
